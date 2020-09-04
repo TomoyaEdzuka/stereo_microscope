1948d98bc315028ef06db520c99fa119061fb1ed
TomoyaEdzuka <eduka.tomoya@j.mbox.nagoya-u.ac.jp>
2020-09-04 16:03:00 +0900

    command test

--- ffmpeg_util.py
+++ ffmpeg_util.py
@@ -2,8 +2,11 @@ import subprocess
 
 
 def render_jpg_to_mp4(jpg_pattern="%05d.jpg", frame_rate=30, out_name='out.mp4'):
-    command_list = ["ffmpeg", "-framerate", str(frame_rate),
+    # command_list = ["ffmpeg", "-framerate", str(frame_rate),
+    #                 "-i", jpg_pattern,
+    #                 "-vcodec", "libx264" "-pix_fmt", "yuv420p",
+    #                 "-r", str(frame_rate), out_name]
+    command_list1 = ["ffmpeg", "-framerate", str(frame_rate),
                     "-i", jpg_pattern,
-                    "-vcodec", "libx264" "-pix_fmt", "yuv420p",
                     "-r", str(frame_rate), out_name]
     subprocess.run(command_list)
