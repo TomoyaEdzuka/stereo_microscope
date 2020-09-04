a83048d7dfb4a1456345ebc55dc69c158c83e6c0
TomoyaEdzuka <eduka.tomoya@j.mbox.nagoya-u.ac.jp>
2020-09-04 16:06:07 +0900

    variable error corrected

--- ffmpeg_util.py
+++ ffmpeg_util.py
@@ -9,4 +9,4 @@ def render_jpg_to_mp4(jpg_pattern="%05d.jpg", frame_rate=30, out_name='out.mp4')
     command_list1 = ["ffmpeg", "-framerate", str(frame_rate),
                     "-i", jpg_pattern,
                     "-r", str(frame_rate), out_name]
-    subprocess.run(command_list)
+    subprocess.run(command_list1)
